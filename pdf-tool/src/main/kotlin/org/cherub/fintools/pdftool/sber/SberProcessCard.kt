c220d9bb78645fb974121c076aa0e311c3d6e1bd
cherub-rus <cherub.rus@mail.ru>
2023-06-30 22:11:18 +0700

    code cleanup

--- pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessCard.kt
+++ pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessCard.kt
@@ -13,8 +13,6 @@ class SberProcessCard : SberProcessor() {
 
     override fun transformToCsv(row: String) = row
         .replace(
-//            "<p><b>([0-9.]{10}) ([0-9:]{5})</b>.{12,17}</p><p><b>(.+)</b>(.+)</p><p><b>([+-]?[0-9,]+)( |</b>[0-9,]+ \\$</p><p><b>)(-?[0-9,]+)</b></p>".toRegex(),
-//            "$1\t$4\t-$5\t\t\t\t$3\t$2:00\t\t\t$7\t=ОКРУГЛ(R[-1]C[-1]+RC[-9];2)\t$4"
             "<p><b>([0-9.]{10}) ([0-9:]{5})</b>.{12,17}</p><p><b>(.+)</b>(.+)</p><p><b>([+-]?[0-9, ]+)</b></p>".toRegex(),
             "$1\t$4\t-$5\t\t\t\t$3\t$2:00\t\t\t$formula_c11\t$formula_c12\t$4"
         )
--- pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessDeposit.kt
+++ pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessDeposit.kt
@@ -15,8 +15,6 @@ class SberProcessDeposit : SberProcessor() {
 
     override fun transformToCsv(row: String) = row
         .replace(
-//            "<p><b>([0-9.]{10}) ([0-9:]{5})</b>.{12,17}</p><p><b>(.+)</b>(.+)</p><p><b>(-?[0-9,]+) (-?[0-9,]+)</b></p>".toRegex(),
-//            "$1\t$3\t$5\t\t\t\t$4\t$2:00\t\t\t$6\t=ОКРУГЛ(R[-1]C[-1]+RC[-9];2)\t$3"
             "<p><b>([0-9.]{10}) (.+?)</b></p><p>(.+?)<b>[0-9] ([+-]?[0-9 ]+,[0-9]{2}) ([0-9 ]+,[0-9]{2})</b></p>".toRegex(),
             "$1\t$3\t-$4\t\t\t\t$2\t00:00\t\t\t$5\t$formula_c12\t$3"
         )
