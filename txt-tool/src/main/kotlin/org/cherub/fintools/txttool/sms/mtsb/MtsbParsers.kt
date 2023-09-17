00c7d525d5d173b9b8fa31fceefb00a1184eb579
cherub-rus <cherub.rus@mail.ru>
2023-09-17 16:40:46 +0700

    code cleanup and formating

--- txt-tool/src/main/kotlin/org/cherub/fintools/txttool/sms/mtsb/MtsbParsers.kt
+++ txt-tool/src/main/kotlin/org/cherub/fintools/txttool/sms/mtsb/MtsbParsers.kt
@@ -4,10 +4,16 @@ import org.cherub.fintools.config.ConfigData
 import org.cherub.fintools.txttool.*
 import org.cherub.fintools.txttool.sms.IContentParser
 
-private const val MTS_BANK_SENDER = "MTS-Bank"
-val mtsbParsers = Pair(MTS_BANK_SENDER, listOf(MtsbParser1(), MtsbParser2(), MtsbParser3()))
+val mtsbParsers = Pair(
+    "MTS-Bank",
+    listOf(
+        MtsbParserMain(),
+        MtsbParserAccountTransfer(),
+        MtsbParserCardTfansfer()
+    )
+)
 
-class MtsbParser1 : IContentParser {
+class MtsbParserMain : IContentParser {
     override fun parse(content: String, config: ConfigData): Transaction? {
         @Suppress("SpellCheckingInspection")
         val regex =
@@ -24,24 +30,24 @@ class MtsbParser1 : IContentParser {
     }
 }
 
-class MtsbParser2 : IContentParser {
-     override fun parse(content: String, config: ConfigData): Transaction? {
+class MtsbParserAccountTransfer : IContentParser {
+    override fun parse(content: String, config: ConfigData): Transaction? {
         @Suppress("SpellCheckingInspection")
         val regex =
             "(?<operation>[^0-9]+) (?<account>[*][0-9]{4}); (?<amount>[0-9][0-9 ]*,[0-9]{2}) RUB; Ostatok: (?<balance>[0-9][0-9 ]*,[0-9]{2}) RUB".toRegex()
         val m = regex.matchEntire(content) ?: return null
 
-         return Transaction(
-             m.gv("account"),
-             m.gv("operation"),
-             "",
-             getAmount(m.gv("amount"), m.gv("operation"), config),
-             m.gv("balance").replace(" ", "")
-         )
+        return Transaction(
+            m.gv("account"),
+            m.gv("operation"),
+            "",
+            getAmount(m.gv("amount"), m.gv("operation"), config),
+            m.gv("balance").replace(" ", "")
+        )
     }
 }
 
-class MtsbParser3 : IContentParser {
+class MtsbParserCardTfansfer : IContentParser {
     override fun parse(content: String, config: ConfigData): Transaction? {
         @Suppress("SpellCheckingInspection")
         val regex =
