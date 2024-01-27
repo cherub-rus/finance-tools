d54483cc1971e949348ca184638eb000b5daf9c9
cherub-rus <cherub.rus@mail.ru>
2024-01-27 19:56:11 +0700

    refactoring code moved to successor

--- pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessPayAcc.kt
+++ pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessPayAcc.kt
@@ -1,8 +1,11 @@
 package org.cherub.fintools.pdftool.sber
 
 import org.cherub.fintools.config.ConfigData
-import org.cherub.fintools.pdftool.formula_c11
-import org.cherub.fintools.pdftool.formula_c12
+import org.cherub.fintools.pdftool.*
+
+private const val SB_ACCOUNT_REGEX = "<p>Счёт Валюта Доступно на (?<currentDate>\\d{2}\\.\\d{2}\\.\\d{4})<b>(?<account>40817 810 \\d \\d{4} \\d{7}) РУБЛЬ РФ (?<currentBalance>(\\d{1,3} )?\\d{1,3},\\d{2})</b></p>"
+private const val SB_REPORT_DATES_REGEX = "<p><b>ОСТАТОК НА (?<startDate>\\d{2}\\.\\d{2}\\.\\d{4}) ОСТАТОК НА (?<endDate>\\d{2}\\.\\d{2}\\.\\d{4}) ВСЕГО СПИСАНИЙ ВСЕГО ПОПОЛНЕНИЙ</b></p>"
+private const val SB_REPORT_BALANCES_REGEX = "<p><b>(?<startBalance>(\\d{1,3} )?\\d{1,3},\\d{2}) (?<endBalance>(\\d{1,3} )?\\d{1,3},\\d{2}) ((\\d{1,3} )?\\d{1,3},\\d{2}) ((\\d{1,3} )?\\d{1,3},\\d{2})</b></p>"
 
 class SberProcessPayAcc(config: ConfigData) : SberProcessor(config) {
 
@@ -17,4 +20,28 @@ class SberProcessPayAcc(config: ConfigData) : SberProcessor(config) {
             "<p><b>([0-9.]{10}) ([0-9:]{5})</b>.{12,17}</p><p><b>(.+)</b>(.+)</p><p><b>(-?[0-9, ]+)</b></p>".toRegex(),
             "$1\t$3\t$5\t\t\t\t$4\t$2:00\t\t\t$formula_c11\t$formula_c12\t$3"
         )
+
+    override fun discoverAccountInfo(text: String): AccountInfo {
+
+        val mAcc = SB_ACCOUNT_REGEX.toRegex().matchEntire(text.lines()[3])
+        val mDates = SB_REPORT_DATES_REGEX.toRegex().matchEntire(text.lines()[5])
+        val mBal = SB_REPORT_BALANCES_REGEX.toRegex().matchEntire(text.lines()[6])
+
+        val number = mAcc?.gv("account")
+        val code =  number?.let {
+            val s = it.replace(" ", "").takeLast(4)
+            config.accounts.findByAccountNumber("СЧЁТ$s")?.code
+        }
+
+        return AccountInfo(
+            accountCode = code,
+            accountNumber = number,
+            startDate = mDates?.gv("startDate"),
+            endDate = mDates?.gv("endDate"),
+            currentDate = mAcc?.gv("currentDate"),
+            startBalance = mBal?.gv("startBalance"),
+            endBalance = mBal?.gv("endBalance"),
+            currentBalance = mAcc?.gv("currentBalance")
+        )
+    }
 }
--- pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessor.kt
+++ pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessor.kt
@@ -6,37 +6,9 @@ import org.cherub.fintools.pdftool.CommonProcessor
 import org.cherub.fintools.pdftool.findByAccountNumber
 import org.cherub.fintools.pdftool.gv
 
-private const val SB_ACCOUNT_REGEX = "<p>Счёт Валюта Доступно на (?<currentDate>\\d{2}\\.\\d{2}\\.\\d{4})<b>(?<account>40817 810 \\d \\d{4} \\d{7}) РУБЛЬ РФ (?<currentBalance>(\\d{1,3} )?\\d{1,3},\\d{2})</b></p>"
-private const val SB_REPORT_DATES_REGEX = "<p><b>ОСТАТОК НА (?<startDate>\\d{2}\\.\\d{2}\\.\\d{4}) ОСТАТОК НА (?<endDate>\\d{2}\\.\\d{2}\\.\\d{4}) ВСЕГО СПИСАНИЙ ВСЕГО ПОПОЛНЕНИЙ</b></p>"
-private const val SB_REPORT_BALANCES_REGEX = "<p><b>(?<startBalance>(\\d{1,3} )?\\d{1,3},\\d{2}) (?<endBalance>(\\d{1,3} )?\\d{1,3},\\d{2}) ((\\d{1,3} )?\\d{1,3},\\d{2}) ((\\d{1,3} )?\\d{1,3},\\d{2})</b></p>"
-
 abstract class SberProcessor(config: ConfigData) : CommonProcessor(config) {
 
     override fun cleanUpHtml(text: String) = super.cleanUpHtml(text)
         .replace("(В валюте счёта</p>)".toRegex(), "$1\n")
         .replace("(<p>Сумма в валюте операции²</p>)".toRegex(), "$1\n")
-
-    override fun discoverAccountInfo(text: String): AccountInfo {
-
-        val mAcc = SB_ACCOUNT_REGEX.toRegex().matchEntire(text.lines()[3])
-        val mDates = SB_REPORT_DATES_REGEX.toRegex().matchEntire(text.lines()[5])
-        val mBal = SB_REPORT_BALANCES_REGEX.toRegex().matchEntire(text.lines()[6])
-
-        val number = mAcc?.gv("account")
-        val code =  number?.let {
-            val s = it.replace(" ", "").takeLast(4)
-            config.accounts.findByAccountNumber("СЧЁТ$s")?.code
-        }
-
-        return AccountInfo(
-            accountCode = code,
-            accountNumber = number,
-            startDate = mDates?.gv("startDate"),
-            endDate = mDates?.gv("endDate"),
-            currentDate = mAcc?.gv("currentDate"),
-            startBalance = mBal?.gv("startBalance"),
-            endBalance = mBal?.gv("endBalance"),
-            currentBalance = mAcc?.gv("currentBalance")
-        )
-    }
 }
