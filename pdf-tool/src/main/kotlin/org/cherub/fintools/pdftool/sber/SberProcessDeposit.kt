58885333598829bd269212e0a5392cb8d87d6056
cherub-rus <cherub.rus@mail.ru>
2024-01-28 23:49:15 +0700

    report head parsing. Sber Deposits

--- pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessDeposit.kt
+++ pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessDeposit.kt
@@ -1,7 +1,13 @@
 package org.cherub.fintools.pdftool.sber
 
 import org.cherub.fintools.config.ConfigData
+import org.cherub.fintools.pdftool.AccountInfo
+import org.cherub.fintools.pdftool.findByAccountNumber
 import org.cherub.fintools.pdftool.formula_c12
+import org.cherub.fintools.pdftool.gv
+
+private val SB_ACCOUNT_REGEX_D = ".+<p>Номер счёта<b>(?<account>40817 810 \\d \\d{4} \\d{7})</b></p>.+".toRegex()
+private val SB_REPORT_INFO_REGEX = "<p><b>Остаток средств</b>на (?<startDate>\\d{2}\\.\\d{2}\\.\\d{4})</p><p><b>(?<startBalance>(\\d{1,3} )?\\d{1,3},\\d{2}) Остаток средств</b>на (?<endDate>\\d{2}\\.\\d{2}\\.\\d{4})</p><p><b>(?<endBalance>(\\d{1,3} )?\\d{1,3},\\d{2})</b></p>".toRegex()
 
 class SberProcessDeposit(config: ConfigData) : SberProcessor(config) {
 
@@ -21,4 +27,25 @@ class SberProcessDeposit(config: ConfigData) : SberProcessor(config) {
         )
         .replace("-+", "")
         .replace("--", "-")
+
+    override fun discoverAccountInfo(text: String): AccountInfo {
+
+        val mAcc = SB_ACCOUNT_REGEX_D.matchAt(text.lines()[2], 0)
+        val mRInfo = SB_REPORT_INFO_REGEX.matchEntire(text.lines()[3])
+
+        val number = mAcc?.gv("account")
+        val code =  number?.let {
+            val s = it.replace(" ", "").takeLast(4)
+            config.accounts.findByAccountNumber("*$s")?.code
+        }
+
+        return AccountInfo(
+            accountCode = code,
+            accountNumber = number,
+            startDate = mRInfo?.gv("startDate"),
+            endDate = mRInfo?.gv("endDate"),
+            startBalance = mRInfo?.gv("startBalance"),
+            endBalance = mRInfo?.gv("endBalance"),
+        )
+    }
 }
