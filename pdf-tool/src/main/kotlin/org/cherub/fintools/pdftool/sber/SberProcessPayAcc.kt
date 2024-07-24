f663630fda8cee8e0056995829dddd74e19a6b8c
cherub-rus <cherub.rus@mail.ru>
2024-07-24 18:04:44 +0700

    parsing update

--- pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessPayAcc.kt
+++ pdf-tool/src/main/kotlin/org/cherub/fintools/pdftool/sber/SberProcessPayAcc.kt
@@ -3,9 +3,10 @@ package org.cherub.fintools.pdftool.sber
 import org.cherub.fintools.config.ConfigData
 import org.cherub.fintools.pdftool.*
 
-private val SB_ACCOUNT_REGEX = "<p>Счёт Валюта Доступно на (?<currentDate>\\d{2}\\.\\d{2}\\.\\d{4})<b>(?<account>40817 810 \\d \\d{4} \\d{7}) РУБЛЬ РФ (?<currentBalance>(\\d{1,3} )?\\d{1,3},\\d{2})</b></p>".toRegex()
-internal val SB_REPORT_DATES_REGEX = "<p><b>ОСТАТОК НА (?<startDate>\\d{2}\\.\\d{2}\\.\\d{4}) ОСТАТОК НА (?<endDate>\\d{2}\\.\\d{2}\\.\\d{4}) ВСЕГО СПИСАНИЙ ВСЕГО ПОПОЛНЕНИЙ</b></p>".toRegex()
+private val SB_ACCOUNT_REGEX = "<p>Номер счёта Дата открытия счёта Дата закрытия счёта Валюта<b>(?<account>40817 810 \\d \\d{4} \\d{7}) .+</b></p>".toRegex()
+internal val SB_REPORT_DATES_REGEX = "<p><b>ОСТАТОК НА (?<startDate>\\d{2}\\.\\d{2}\\.\\d{4}) ВСЕГО ПОПОЛНЕНИЙ ВСЕГО СПИСАНИЙ ОСТАТОК НА (?<endDate>\\d{2}\\.\\d{2}\\.\\d{4})</b></p>".toRegex()
 internal val SB_REPORT_BALANCES_REGEX = "<p><b>(?<startBalance>(\\d{1,3} )?\\d{1,3},\\d{2}) ((\\d{1,3} )?\\d{1,3},\\d{2}) ((\\d{1,3} )?\\d{1,3},\\d{2}) (?<endBalance>(\\d{1,3} )?\\d{1,3},\\d{2})</b></p>".toRegex()
+private val SB_ACCOUNT_CURRENT_DATE_REGEX = "<p>Дата формирования <b>(?<currentDate>\\d{2}\\.\\d{2}\\.\\d{4})</b></p>".toRegex()
 
 class SberProcessPayAcc(config: ConfigData) : SberProcessor(config) {
 
@@ -30,6 +31,7 @@ class SberProcessPayAcc(config: ConfigData) : SberProcessor(config) {
         val mAcc = SB_ACCOUNT_REGEX.matchEntire(text.lines()[3])
         val mDates = SB_REPORT_DATES_REGEX.matchEntire(text.lines()[5])
         val mBal = SB_REPORT_BALANCES_REGEX.matchEntire(text.lines()[6])
+        val mCur = SB_ACCOUNT_CURRENT_DATE_REGEX.matchEntire(text.lines()[text.lines().size - 2])
 
         val number = mAcc?.gv("account")
         val code =  number?.let {
@@ -42,10 +44,9 @@ class SberProcessPayAcc(config: ConfigData) : SberProcessor(config) {
             accountNumber = number,
             startDate = mDates?.gv("startDate"),
             endDate = mDates?.gv("endDate"),
-            currentDate = mAcc?.gv("currentDate"),
             startBalance = mBal?.gv("startBalance"),
             endBalance = mBal?.gv("endBalance"),
-            currentBalance = mAcc?.gv("currentBalance")
+            currentDate = mCur?.gv("currentDate"),
         )
     }
 }
